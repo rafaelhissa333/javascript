3ad37b97936e7e102a64c57155739c66f1221fbb
rafaelhissa333 <67077738+rafaelhissa333@users.noreply.github.com>
2022-01-16 19:16:54 -0300

    update exercícios da Apostila

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,12 @@
+// var cont = 1
+// while(cont <= 6){
+//     console.log(`Passo ${cont}`)
+//     cont++
+// }
+
+var cont = 1
+do{
+   console.log(`Passo ${cont}`)
+   cont++
+}while(cont <= 6)
+
